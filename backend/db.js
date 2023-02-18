a0fdc171d70c5c67ff2d22862fabbe3cb151802a
HarshBansal2k01 <ihbbro14@gmail.com>
2023-02-18 22:14:49 +0530

    create user

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,21 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  "mongodb+srv://harsh:harsh123@cluster0.8zewixo.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://harsh:harsh123@cluster0.8zewixo.mongodb.net/FoodTopia?retryWrites=true&w=majority";
 
 const mongoDB = async () => {
-  await mongoose.connect(mongoURI, (err, result) => {
+  await mongoose.connect(mongoURI, async (err, result) => {
     if (err) {
       console.log("---", err);
     } else {
       console.log("Connected Successfully");
+      const fetched_data = await mongoose.connection.db.collection(
+        "food_items"
+      );
+      fetched_data.find({}).toArray(function (err, data) {
+        if (err) console.log(err);
+        else console.log();
+      });
     }
   });
 };
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 5000 
+const express = require("express");
+const app = express();
+const port = 5000;
 // importing mongoose
-const mongoDB = require("./db")
+const mongoDB = require("./db");
 mongoDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// middle ware
+app.use(express.json());
+app.use("/api", require("./Routes/CreateUser"));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});

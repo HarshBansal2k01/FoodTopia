e433692b6ef36a78c46bcf1238399b261caa248b
HarshBansal2k01 <ihbbro14@gmail.com>
2023-02-19 13:24:30 +0530

    display

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response = await response.json();
+
+    console.log(response)
+  };
+
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <div>
-        <Carousel/>
+        <Carousel />
       </div>
       <div className="m-3">
-        <Card />
-        <Card />
-        <Card />
         <Card />
       </div>
       <div>
